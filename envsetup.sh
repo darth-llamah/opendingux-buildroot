5b6e7cfbacc9d1ab3f5ce508ab7990bd85575357
darth-llamah <gen_scheisskopf@o2.pl>
2011-10-03 19:55:45 +0200

    more Spica specific stuff

---
+++ envsetup.sh
@@ -0,0 +1,2 @@
+export ARCH=arm
+export INITRAMFS_SOURCE_PATH=/home/matus/i5700/initramfs-3.0
--- package/gmenu2x/gmenu2x.mk
+++ package/gmenu2x/gmenu2x.mk
@@ -4,9 +4,9 @@
 #
 #############################################################
 GMENU2X_VERSION = master
-GMENU2X_SITE = git://projects.qi-hardware.com/gmenu2x.git
+GMENU2X_SITE = git://github.com/darth-llamah/gmenu2x.git
 GMENU2X_DEPENDENCIES = sdl sdl_gfx libpng
 GMENU2X_AUTORECONF = YES
-GMENU2X_CONF_OPT = --with-sdl-prefix=$(STAGING_DIR)/usr --enable-platform=dingux
+GMENU2X_CONF_OPT = --with-sdl-prefix=$(STAGING_DIR)/usr --enable-platform=spica
 
 $(eval $(call AUTOTARGETS,package,gmenu2x))
